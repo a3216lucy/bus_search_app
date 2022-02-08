bb1904edfce6763514e80ef704b59b089d977d25
Kimi <lys.kimi@gms.tku.edu.tw>
2022-02-08 17:40:12 +0800

    Add: finish search view

---
+++ index.js
@@ -0,0 +1,35 @@
+const cityData = [
+  { name: '臺北市', value: 'Taipei' },
+  { name: '新北市', value: 'NewTaipei' },
+  { name: '桃園市', value: 'Taoyuan' },
+  { name: '臺中市', value: 'Taichung' },
+  { name: '臺南市', value: 'Tainan' },
+  { name: '高雄市', value: 'Kaohsiung' },
+  { name: '基隆市', value: 'Keelung' },
+  { name: '新竹市', value: 'Hsinchu' },
+  { name: '新竹縣', value: 'HsinchuCounty' },
+  { name: '苗栗縣', value: 'MiaoliCounty' },
+  { name: '彰化縣', value: 'ChanghuaCounty' },
+  { name: '南投縣', value: 'NantouCounty' },
+  { name: '雲林縣', value: 'YunlinCounty' },
+  { name: '嘉義縣', value: 'ChiayiCounty' },
+  { name: '嘉義市', value: 'Chiayi' },
+  { name: '屏東縣', value: 'PingtungCounty' },
+  { name: '宜蘭縣', value: 'YilanCounty' },
+  { name: '花蓮線', value: 'HualienCounty' },
+  { name: '臺東縣', value: 'TaitungCounty' },
+  { name: '金門縣', value: 'KinmenCounty' },
+  { name: '澎湖縣', value: 'PenghuCounty' },
+  { name: '連江縣', value: 'LienchiangCounty' },
+]
+
+// select bus area 
+const citySelect = document.querySelector('#citySelect');
+function select(){
+  let str = '';
+  cityData.forEach((item) => {
+    str += `<option value="${item.value}">${item.name}</option>`
+  })
+  citySelect.innerHTML = str;
+}
+select();
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        "roboto": ["Roboto"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
